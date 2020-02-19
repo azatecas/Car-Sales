2f5400a5e34f6f1c48f2763f8da49211180de970
Luis PM <luisp237@gmail.com>
2020-02-19 16:05:48 -0500

    a lot of bugs and functionality not working

--- src/App.js
+++ src/App.js
@@ -1,48 +1,49 @@
 import React from 'react';
-
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+import { connect } from 'react-redux'
+import { addFeature, removeFeature, addTotal } from './actions/actions'
 
-const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
-
-  const removeFeature = item => {
+const App = ({car, additionalFeatures, additionalPrice, removeFeature, addTotal}) => {
+  // console.table(props);
+  const removeFeatures = item => {
     // dispatch an action here to remove an item
+    removeFeature(item)
+    addTotal(-item.price)
   };
 
   const buyItem = item => {
     // dipsatch an action here to add an item
+    addFeature(item)
+    addTotal(item.price)
   };
 
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={car} />
+        <AddedFeatures car={car} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures additionalFeatures={additionalFeatures} />
+        <Total car={car} additionalPrice={additionalPrice} />
       </div>
     </div>
   );
 };
+function mapStateToProps(state) {
+  console.log('state',state)
+  return {
+    car: state.car,
+    additionalFeatures: state.additionalFeatures,
+    additionalPrice: state.additionalPrice
+  }  
+}
 
-export default App;
+export default connect(mapStateToProps,{
+  addFeature,
+  removeFeature,
+  addTotal
+})(App);
